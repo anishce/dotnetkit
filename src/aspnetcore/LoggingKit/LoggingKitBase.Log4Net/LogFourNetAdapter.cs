b44fa269d99bdb616297882be00e35494f8367ef
Anish <anish.cse7@gmail.com>
2024-05-25 12:38:04 +0530

    Update LogFourNetAdapter.cs

--- src/aspnetcore/LoggingKit/LoggingKitBase.Log4Net/LogFourNetAdapter.cs
+++ src/aspnetcore/LoggingKit/LoggingKitBase.Log4Net/LogFourNetAdapter.cs
@@ -1,17 +1,77 @@
-﻿using System;
+﻿using log4net;
+using System;
 
 namespace LoggingKitBase.Log4Net
 {
     public sealed class LogFourNetAdapter : ILogAdapter
     {
+        private readonly ILog logger;
+
+        public LogFourNetAdapter()
+        {
+            logger = LogManager.GetLogger(typeof(LogFourNetAdapter));
+        }
+
         public void Log(LogLevel logLevel, string message)
         {
-            throw new NotImplementedException();
+            switch (logLevel)
+            {
+                case LogLevel.Error:
+                    this.logger.Error(message);
+                    break;
+
+                case LogLevel.Debug:
+                    this.logger.Debug(message);
+                    break;
+
+                case LogLevel.Information:
+                    this.logger.Info(message);
+                    break;
+
+                case LogLevel.Warning:
+                    this.logger.Warn(message);
+                    break;
+
+                case LogLevel.FATAL:
+                    this.logger.Fatal(message);
+                    break;
+                case LogLevel.Trace:
+                    throw new ArgumentNullException("Invalid log level.");
+
+                default:
+                    throw new ArgumentNullException("Invalid log level.");
+            }
         }
 
         public void Log(LogLevel logLevel, Exception exception)
         {
-            throw new NotImplementedException();
+            switch (logLevel)
+            {
+                case LogLevel.Error:
+                    this.logger.Error(exception);
+                    break;
+
+                case LogLevel.Debug:
+                    this.logger.Debug(exception);
+                    break;
+
+                case LogLevel.Information:
+                    this.logger.Info(exception);
+                    break;
+
+                case LogLevel.Warning:
+                    this.logger.Warn(exception);
+                    break;
+
+                case LogLevel.FATAL:
+                    this.logger.Fatal(exception);
+                    break;
+                case LogLevel.Trace:
+                    throw new ArgumentNullException("Invalid log level.");
+
+                default:
+                    throw new ArgumentNullException("Invalid log level.");
+            }
         }
 
         public void Log(LogLevel logLevel, Exception exception, string message)
