3e66225428c5f61ce16f363544779f50c2fe3b76
Anish CE <anish.cse7@gmail.com>
2024-09-04 20:40:11 +0530

    Update ICacheStrategyAsync.cs

--- src/aspnetcore/CachingKit/CachingKitBase/ICacheStrategyAsync.cs
+++ src/aspnetcore/CachingKit/CachingKitBase/ICacheStrategyAsync.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace CachingKitBase
 {
     public interface ICacheStrategyAsync
     {
-
+        void RemoveAsync(string key);
+        void StoreAsync<T>(string key, T data, TimeSpan? duration = null);
+        void StoreAsync<T>(string key, T data, TimeSpan? absoluteExpireTime = null, TimeSpan? slidingExpireTime = null);
+        T RetrieveAsync<T>(string key);
     }
 }
