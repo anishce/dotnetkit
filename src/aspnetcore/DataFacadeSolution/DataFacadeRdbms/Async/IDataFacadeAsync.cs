ee083a593c4d7514728b9096e498c50d3f4d4074
Anish CE <anish.cse7@gmail.com>
2025-01-12 11:50:46 +0530

    Create IDataFacadeAsync.cs

---
+++ src/aspnetcore/DataFacadeSolution/DataFacadeRdbms/Async/IDataFacadeAsync.cs
@@ -0,0 +1,11 @@
+﻿// ************************************************************************
+// Copyright (c) AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+namespace DataFacadeRdbms.Async
+{
+    internal class IDataFacadeAsync
+    {
+    }
+}
