144cb27744c62fa41bf5ea643d17c610fbc955ff
Anish CE <anish.cse7@gmail.com>
2025-01-14 22:32:14 +0530

    Update ErrorViewModel.cs

--- src/aspnetcore/DataFacadeSolution/DataFacadeSolutionWeb/Models/ErrorViewModel.cs
+++ src/aspnetcore/DataFacadeSolution/DataFacadeSolutionWeb/Models/ErrorViewModel.cs
@@ -1,3 +1,8 @@
+// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
 namespace DataFacadeSolutionWeb.Models
 {
     public class ErrorViewModel
