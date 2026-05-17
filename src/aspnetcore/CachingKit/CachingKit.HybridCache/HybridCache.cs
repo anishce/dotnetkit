77445486a686fd843ba6a67a7bd4f38aa579cfe5
Anish CE <anish.cse7@gmail.com>
2026-05-17 19:05:49 +0530

    Update HybridCache.cs

--- src/aspnetcore/CachingKit/CachingKit.HybridCache/HybridCache.cs
+++ src/aspnetcore/CachingKit/CachingKit.HybridCache/HybridCache.cs
@@ -3,10 +3,30 @@
 // Author: AnishCeDev
 // ************************************************************************
 
+using CachingKitBase;
+
 namespace CachingKit.HybridCache
 {
-    public class HybridCache
+    public class HybridCache : ICacheStrategy
     {
+        public void Remove(string key)
+        {
+            throw new NotImplementedException();
+        }
 
+        public T Retrieve<T>(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Store<T>(string key, T data, TimeSpan? duration = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Store<T>(string key, T data, TimeSpan? absoluteExpireTime = null, TimeSpan? slidingExpireTime = null)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
