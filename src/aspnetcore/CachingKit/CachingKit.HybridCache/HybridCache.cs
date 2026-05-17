6a641c5d19cbe8f85b8ba9e40468ddf6d609f6f0
Anish CE <anish.cse7@gmail.com>
2026-05-17 18:57:10 +0530

    Create HybridCache.cs

---
+++ src/aspnetcore/CachingKit/CachingKit.HybridCache/HybridCache.cs
@@ -0,0 +1,12 @@
+﻿// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+namespace CachingKit.HybridCache
+{
+    public class HybridCache
+    {
+
+    }
+}
