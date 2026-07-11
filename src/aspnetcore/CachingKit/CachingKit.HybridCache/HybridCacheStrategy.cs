8b1cb4a040c70b963927635cae74c058eca4e13a
Anish CE <anish.cse7@gmail.com>
2026-07-11 17:43:44 +0530

    Create HybridCacheStrategy.cs

---
+++ src/aspnetcore/CachingKit/CachingKit.HybridCache/HybridCacheStrategy.cs
@@ -0,0 +1,37 @@
+﻿// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using CachingKitBase;
+
+namespace CachingKit.HybridCache
+{
+    public class HybridCacheStrategy : ICacheStrategy
+    {
+        public HybridCacheStrategy()
+        {
+            
+        }
+
+        public void Remove(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Retrieve<T>(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Store<T>(string key, T data, TimeSpan? duration = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Store<T>(string key, T data, TimeSpan? absoluteExpireTime = null, TimeSpan? slidingExpireTime = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
