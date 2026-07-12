9c3b9fc065b3690b313f7e2d186b1a775938ff8f
Anish CE <anish.cse7@gmail.com>
2026-07-12 18:13:59 +0530

    Create MemoryCachingExtensions.cs

---
+++ src/aspnetcore/CachingKit/CachingKit.InMemory/MemoryCachingExtensions.cs
@@ -0,0 +1,23 @@
+﻿// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using CachingKitBase;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CachingKit.InMemory
+{
+    public static class MemoryCachingExtensions
+    {
+        public static void AddInMemoryCaching(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<ICacheStrategy, InMemoryCacheStrategy>();
+            // Register the HybridCacheStrategy as the implementation of ICacheStrategy
+            //services.AddScoped<ICacheStrategy, HybridCache>();
+            // Optionally, you can also register any dependencies required by HybridCacheStrategy here
+            // For example, if HybridCacheStrategy depends on other services, you can register them as well
+        }
+    }
+}
