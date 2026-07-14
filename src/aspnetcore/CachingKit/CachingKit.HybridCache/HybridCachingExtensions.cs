fdf57b7d613f165cfb30569362374dc9adff0694
Anish CE <anish.cse7@gmail.com>
2026-07-14 18:04:43 +0530

    Update HybridCachingExtensions.cs

--- src/aspnetcore/CachingKit/CachingKit.HybridCache/HybridCachingExtensions.cs
+++ src/aspnetcore/CachingKit/CachingKit.HybridCache/HybridCachingExtensions.cs
@@ -12,7 +12,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CachingKit.HybridCache
+namespace CachingKit.HybridCaching
 {
     public static class HybridCachingExtensions
     {
