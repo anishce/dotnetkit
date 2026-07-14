b54413b2a55c993e8e29f9f76ca93aed1e9d9b24
Anish CE <anish.cse7@gmail.com>
2026-07-14 17:26:59 +0530

    Create MemCachedExtensions.cs

---
+++ src/aspnetcore/CachingKit/CachingKit.MemCached/MemCachedExtensions.cs
@@ -0,0 +1,27 @@
+﻿// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using Enyim.Caching.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CachingKit.MemCached
+{
+    public static class MemCachedExtensions
+    {
+        public static IServiceCollection AddMemcachedCaching(this IServiceCollection services, IConfiguration configuration)
+        {
+            string cachingServer = configuration.GetValue<string>("Caching:Memcached:ServerHost") ?? string.Empty;
+            int cachingServerPort = configuration.GetValue<int>("Caching:Memcached:ServerPort");
+
+            return services.AddEnyimMemcached(o => o.Servers = new List<Server> {
+                new Server {
+                    Address = cachingServer,
+                    Port = cachingServerPort
+                }
+            });
+        }
+    }
+}
