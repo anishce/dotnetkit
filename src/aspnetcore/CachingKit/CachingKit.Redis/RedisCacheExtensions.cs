810b2a0f1691b16e3be2af6169bacb3373b39eaf
Anish CE <anish.cse7@gmail.com>
2026-07-14 17:27:04 +0530

    Create RedisCacheExtensions.cs

---
+++ src/aspnetcore/CachingKit/CachingKit.Redis/RedisCacheExtensions.cs
@@ -0,0 +1,24 @@
+﻿// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CachingKit.Redis
+{
+    public static class RedisCacheExtensions
+    {
+        public static IServiceCollection AddRedisCache(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = configuration.GetConnectionString("RedisCacheConnection");
+                options.InstanceName = configuration.GetValue<string>("Caching:Redis:InstanceName") ?? string.Empty;
+            });
+
+            return services;
+        }
+    }
+}
