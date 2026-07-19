e0c8ebb817c3c9cf16a1784c17135e82e526a976
Anish CE <anish.cse7@gmail.com>
2026-07-19 17:17:46 +0530

    Create AsyncSqlServerDataFacade.cs

---
+++ src/aspnetcore/DataFacadeSolution/DataFacadeRdbms/Async/AsyncSqlServerDataFacade.cs
@@ -0,0 +1,80 @@
+﻿// ************************************************************************
+// Copyright (c) 2026 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace DataFacadeRdbms.Async
+{
+    public class AsyncSqlServerDataFacade : IAsyncDataFacade
+    {
+        public void AddParameter(IDbCommand cmd, string param, DbParamType dbParamType, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddParameter(IDbCommand cmd, string param, DbParamType dbParamType, ParameterDirection paramDirection)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddParameter(IDbCommand cmd, string param, DbParamType dbParamType, int size, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CloseConnectionAsync(IDbConnection conn)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ExecuteNonQueryAsync(IDbCommand cmd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataReader ExecuteReaderAsync(IDbCommand cmd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataReader ExecuteReaderAsync(IDbCommand cmd, CommandBehavior cmdBehavior)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ExecuteScalarAsync(IDbCommand cmd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDbCommand GetDbCommandAsync(string commandText, IDbConnection conn)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDbCommand GetDbCommandAsync(string commandText, IDbConnection conn, IDbTransaction tran)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDbConnection GetDbConnectionAsync(string connectionString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDbTransaction GetTransactionAsync(IDbConnection conn)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object? SafeConvertFromDBNull(IDataReader reader, string columnName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
